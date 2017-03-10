efa64f23ebe98792bd466109aacf5a2cd34ebde8
Melanie Myers <melanie.myers@365smartshop.com>
2017-03-10 11:26:28 -0500

    changed to file input

--- src/reverseWords/main.java
+++ src/reverseWords/main.java
@@ -5,26 +5,48 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class main {
-	static String startSentence = "this is my sample";
 
 	public static void main(String[] args) throws IOException {
-		separateSentence(startSentence);
+		Scanner myScanner = loadFile();
+		int myNumOfCases = pullCaseNumber (myScanner) + 1;
+
+		for (int i=0; i < myNumOfCases; i++) {
+			String mySentence = pullSentence(myScanner);
+			separateSentence(mySentence);
+		}
+	}
+
+	public static Scanner loadFile() throws IOException{
+		File incomingFile = new File("B-small-practice.txt");
+		Scanner scan = new Scanner(incomingFile);
+		return scan;
+	}
+
+	public static int pullCaseNumber(Scanner scan){
+		int numOfCases = scan.nextInt();
+		System.out.println("Number of cases:" + numOfCases);
+		return numOfCases;
+	}
+
+	public static String pullSentence( Scanner scan ){
+		String nextSentence = scan.nextLine();
+		return nextSentence;
 	}
 
 	public static void separateSentence(String sentence) throws IOException{
 		String[] splitSentence = sentence.split(" ");
-		printArray(splitSentence);
 		printArrayBackwards(splitSentence, createOutputFile());
 	}
-	
-	
+
+
 	public static PrintWriter createOutputFile() throws IOException{
-	File outFile = new File ("output.txt");
-	FileWriter fWriter = new FileWriter (outFile);
-	PrintWriter pWriter = new PrintWriter (fWriter);
-	return pWriter;
+		File outFile = new File ("output.txt");
+		FileWriter fWriter = new FileWriter (outFile);
+		PrintWriter pWriter = new PrintWriter (fWriter);
+		return pWriter;
 	}
 
 
@@ -33,7 +55,9 @@ public class main {
 			System.out.println(myArray[i]);
 		}
 	}
-	
+
+
+
 	public static void printArrayBackwards(String[] myArray, PrintWriter outputWriter){
 		for (int i= (myArray.length-1); i > -1; i-- ) {
 			System.out.print(myArray[i] + " ");
